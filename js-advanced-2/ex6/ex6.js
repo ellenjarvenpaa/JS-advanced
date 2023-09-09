16a4e7e6af32b2e1fa8dc65c38dd97693ca16d74
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2023-09-09 11:55:26 +0300

    valmis

---
+++ js-advanced-2/ex6/ex6.js
@@ -0,0 +1,20 @@
+// Sample expenses array
+const expenses = [
+  { category: "Food", amount: 50 },
+  { category: "Transportation", amount: 30 },
+  { category: "Entertainment", amount: 20 },
+];
+
+// TODO: Implement the calculateTotalExpenses function
+function calculateTotalExpenses(expenses) {
+  // TODO: Use the reduce function to calculate total expenses
+  const total = expenses.reduce((total, expense) => total + expense.amount, 0);
+  return total;
+}
+
+// Test the calculateTotalExpenses function
+const totalExpenses = calculateTotalExpenses(expenses);
+console.log(totalExpenses);
+
+// Output should be:
+// 100
